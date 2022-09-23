98ac173abac80f7959e0da140e7eebf7397faef7
Brian Lucius <brian@brianlucius.net>
2022-09-23 00:19:13 -0600

    UI Fixes

--- client/src/components/PortfolioTable.js
+++ client/src/components/PortfolioTable.js
@@ -1,27 +1,28 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import { alpha } from '@mui/material/styles';
+import { alpha, emphasize } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
+import TableHead, { tableHeadClasses } from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import TableSortLabel from '@mui/material/TableSortLabel';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
+import { blue, grey } from '@mui/material/colors';
 import { visuallyHidden } from '@mui/utils';
 import moment from 'moment';
+import { borderBottom, borders } from '@mui/system';
 const _ = require('lodash');
 
 const formatCurrency = new Intl.NumberFormat('en-US', {
@@ -170,7 +171,7 @@ function EnhancedTableHead(props) {
   };
 
   return (
-    <TableHead>
+    <TableHead sx={{backgroundColor: blue[200], borderBottom: 2, borderColor: 'grey'}}>
       <TableRow>
         {headCells.map((headCell) => (
           <TableCell
@@ -380,13 +381,18 @@ export default function PortfolioTable(props) {
                     height: (dense ? 33 : 53) * emptyRows,
                   }}
                 >
-                  <TableCell colSpan={6} />
+                  <TableCell colSpan={12} />
                 </TableRow>
               )}
             </TableBody>
           </Table>
         </TableContainer>
         <TablePagination
+          sx={{".MuiTablePagination-selectLabel": {
+                mb: 0,},
+               ".MuiTablePagination-displayedRows": {
+                mb: 0,},
+              }}
           rowsPerPageOptions={[5, 10, 25]}
           component="div"
           count={rows.length}
